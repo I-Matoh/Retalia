0234842a4eff4fc41c466cb5917bb692306aecec
ItsMato <matrixjmax@outlook.com>
2025-09-16 14:26:36 +0300

    Update add.tsx

--- app/add.tsx
+++ app/add.tsx
@@ -5,4 +5,4 @@ import { Redirect } from 'expo-router';
 // This is a redirect to the tab-based add transaction screen
 export default function AddScreen() {
   return <Redirect href="/(tabs)/add" />;
-}
+} 
